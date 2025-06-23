7794b0d9efa58eff985bc6689842ea840aeeb835
josephadogeriopao <joseph.adogeri@orleansassessors.com>
2025-06-23 16:22:54 -0500

    Removed server directory

--- src/configs/cors.js
+++ src/configs/cors.js
@@ -3,6 +3,11 @@ const corsOptions = {
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    //origin: 'http://localhost:3100', // Replace with your React app's origin
+    // Allow specific methods
+    // Allow specific headers
+    allowedHeaders: "Content-Type, Authorization",
+    credentials: true, // Allow cookies to be sent
 }
 
 module.exports = {corsOptions};
--- src/configs/mongoDB.js
+++ src/configs/mongoDB.js
@@ -3,12 +3,15 @@ const mongoose = require('mongoose');
 // Connect to MongoDB locally
 
 const startConnection =()=>{
-mongoose.connect('mongodb://localhost:27017/testDB', {
-    // useNewUrlParser: true,
-    // useUnifiedTopology: true,
-})
-.then(() => console.log('Connected to MongoDB'))
-.catch(err => console.error('MongoDB connection error:', err));
+    // Name your database as shown below
+    //const db = 'mongodb://localhost:27017/your_database_name';
+    const db = 'mongodb://localhost:27017/';
+    mongoose.connect(db, {
+        // useNewUrlParser: true,
+        // useUnifiedTopology: true,
+    })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('MongoDB connection error:', err));
 }
 
 module.exports = { startConnection }
