4e16263c8a10f9fdee90b2cb5049213aa8dcf804
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-06-25 17:42:03 -0500

    Updated mongodb url

--- src/configs/mongoDB.js
+++ src/configs/mongoDB.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
-
 // Connect to MongoDB locally
 
 const startConnection =()=>{
     // Name your database as shown below
     //const db = 'mongodb://localhost:27017/your_database_name';
-    const db = 'mongodb://localhost:27017/';
+    const db = process.env.MONGODB_URL;
     mongoose.connect(db, {
         // useNewUrlParser: true,
         // useUnifiedTopology: true,
